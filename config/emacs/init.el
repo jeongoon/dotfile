5b482b50ef49598a5b30777184f96bf3fec6368f
Myoungjin JEON <jeongoon@gmail.com>
2020-05-16 12:00:56 +1000

    [Emacs:init] change diminish rules

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -58,6 +58,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#839496" :family "Fira Sans Compressed" :height 1.5 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#839496" :family "Fira Sans Compressed" :height 1.5))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#839496" :family "Fira Sans Compressed" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#839496" :family "Fira Sans Compressed" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#839496" :family "Fira Sans Compressed" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#839496" :family "Fira Sans Compressed"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#839496" :family "Fira Sans Compressed"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#839496" :family "Fira Sans Compressed"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#839496" :family "Fira Sans Compressed")))))
 
 ;;; init.el ends here
